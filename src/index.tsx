0881eb60c85bc4f8f078034ec07c4c2a58065ba1
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2025-03-07 16:48:37 -0300

    Update some dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,6 @@ import App from './App';
 import ReactGA from 'react-ga4';
 ReactGA.initialize('G-R47V0VDB3D');
 
-// import initReactFastclick from 'react-fastclick';
-// initReactFastclick();
-
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
